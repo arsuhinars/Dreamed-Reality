5804a6abdaacd0bee295e1f315b9e4e1e1e42aa9
arsuhinars <arsuhinars@gmail.com>
2023-07-22 16:15:50 +0400

    Scriptable classes updates

---
+++ Assets/Scripts/Scriptables/MovableTweenerSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace DreamedReality.Scriptables
+{
+    [CreateAssetMenu(fileName = "MovableTweenerSettings", menuName = "Game/Tweens/Movable Tweener Settings")]
+    public class MovableTweenerSettings : ScriptableObject
+    {
+        public Vector3 slideOffset;
+        public float slideDuration;
+    }
+}
---
+++ Assets/Scripts/Scriptables/RotatableTweenerSetting.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace DreamedReality.Scriptables
+{
+    [CreateAssetMenu(fileName = "RotatableTweenerSettings", menuName = "Game/Tweens/Rotatable Tweener Settings")]
+    public class RotatableTweenerSetting : ScriptableObject
+    {
+        public Vector3 rotationAngle;
+        public float rotationDuration;
+    }
+}
